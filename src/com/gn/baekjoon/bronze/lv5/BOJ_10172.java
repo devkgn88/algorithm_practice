645ecba5d3c2f3030e3bc775507dacab09011d46
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:34:39 +0900

    [김가남] 백준 10172 개 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10172.java
@@ -0,0 +1,17 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10172
+ * 문제명: 개
+ * 문제 링크: https://www.acmicpc.net/problem/10172
+ */
+public class BOJ_10172 {
+	public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+	}
+}
