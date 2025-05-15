abba6aef12c21b481e941231d8cc93deea94ed49
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:31:06 +0900

    [김가남] 백준 10171 고양이 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10171.java
@@ -0,0 +1,16 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10171
+ * 문제명: 고양이
+ * 문제 링크: https://www.acmicpc.net/problem/10171
+ */
+public class BOJ_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
