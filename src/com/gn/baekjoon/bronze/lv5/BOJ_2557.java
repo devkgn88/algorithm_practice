baeaea2d2af623a2020adca82f8d5f7b0129155a
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:11:15 +0900

    [김가남] 백준 브론즈5 2557 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_2557.java
@@ -0,0 +1,12 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 문제 번호: 2557
+ * 문제명: Hello World
+ * 문제 링크: https://www.acmicpc.net/problem/2557
+ */
+public class BOJ_2557 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
