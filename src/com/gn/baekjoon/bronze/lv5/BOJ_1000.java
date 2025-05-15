dfa2ba84f1dd7fa22106ee8f26cfe2318057cebf
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:47:51 +0900

    [김가남] 백준 1000번 A+B 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_1000.java
@@ -0,0 +1,19 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.util.Scanner;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 1000
+ * 문제명: A+B
+ * 문제 링크: https://www.acmicpc.net/problem/1000
+ */
+public class BOJ_1000 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+}
