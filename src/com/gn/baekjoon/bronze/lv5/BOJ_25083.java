b51f0ebf849180baee3ba81c545071bb0ec2de14
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:44:25 +0900

    [김가남] 백준 25083번 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_25083.java
@@ -0,0 +1,18 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 25083
+ * 문제명: 새싹
+ * 문제 링크: https://www.acmicpc.net/problem/25083
+ */
+public class BOJ_25083 {
+	public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+	}
+}
