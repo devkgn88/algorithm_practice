eff76cfce44619ae9fd6eb00e30744cad174da4e
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:52:31 +0900

    [김가남] 백준 10998 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10998.java
@@ -0,0 +1,19 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.util.Scanner;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10998
+ * 문제명: AxB
+ * 문제 링크: https://www.acmicpc.net/problem/10998
+ */
+public class BOJ_10998 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+        sc.close();
+    }
+}
