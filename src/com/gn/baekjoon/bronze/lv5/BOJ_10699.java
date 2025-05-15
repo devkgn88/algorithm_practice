00593fe69d2cd187c27cce47e8af4e734acc1b58
devkgn88 <devkgn88@gmail.com>
2025-05-15 12:14:08 +0900

    [김가남] 브론즈 5 10699 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10699.java
@@ -0,0 +1,17 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 문제 번호: 10699
+ * 문제명: Hello World
+ * 문제 링크: https://www.acmicpc.net/problem/10699
+ */
+public class BOJ_10699 {
+    public static void main(String[] args){
+        LocalDate ld = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(ld.format(formatter));
+    }
+}
