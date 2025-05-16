cbac58d5586c17bf34b605ced1c2687697d2a933
devkgn88 <devkgn88@gmail.com>
2025-05-16 17:33:02 +0900

    [김가남] 백준 5597 과제 안 내신 분 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv3/BOJ_5597.java
@@ -0,0 +1,37 @@
+package com.gn.baekjoon.bronze.lv3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 5597
+ * 문제명: 과제 안 내신 분...?
+ * 문제 링크: https://www.acmicpc.net/problem/5597
+ */
+public class BOJ_5597 {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));){
+			int[] arr = new int[30];
+			String line;
+			while((line = br.readLine()) != null && !line.isEmpty()) {
+				int num = Integer.parseInt(line.trim());
+				arr[num-1] = 1;
+			}
+			
+			for(int i = 0 ; i < arr.length ; i++) {
+				if(arr[i] == 0) {
+					bw.write(String.valueOf(i+1));
+					bw.newLine();
+				}
+			}
+			bw.flush();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
