4cdae20d1bd047f3ee0cff8708b5fbb5803695c7
devkgn88 <devkgn88@gmail.com>
2025-05-16 19:44:37 +0900

    [김가남] 백준 2744 대소문자 바꾸기 문제 풀이

--- src/com/gn/baekjoon/bronze/lv5/BOj_2744.java
+++ src/com/gn/baekjoon/bronze/lv5/BOj_2744.java
@@ -7,17 +7,24 @@ import java.io.OutputStreamWriter;
 
 /**
  * 레벨 : 브론즈 5
- * 문제 번호: 2743
- * 문제명: 단어 길이 재기
- * 문제 링크: https://www.acmicpc.net/problem/2743
+ * 문제 번호: 2744
+ * 문제명: 대소문자 바꾸기
+ * 문제 링크: https://www.acmicpc.net/problem/2744
  */
-public class BOJ_2743 {
+public class BOj_2744 {
 	public static void main(String[] args) {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));){
-			String str = br.readLine();
-			int length = str.length();
-			bw.write(String.valueOf(length));
+			String str = br.readLine().trim();
+			char[] arr = str.toCharArray();
+			for(int i = 0 ; i < arr.length ; i++) {
+				int target = (int)arr[i];
+				if(97 <= target && target <= 122) {
+					bw.write((char)(target-32));
+				} else if(65 <= target && target <= 90) {
+					bw.write((char)(target+32));;
+				}
+			}
 			bw.flush();
 		}catch(Exception e) {
 			e.printStackTrace();
