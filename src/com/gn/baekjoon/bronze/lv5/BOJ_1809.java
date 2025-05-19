b94c922c659552d52154526abc45e4596bb7033a
devkgn88 <devkgn88@gmail.com>
2025-05-19 12:34:57 +0900

    [김가남] 백준 1809 Moo 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_1809.java
@@ -0,0 +1,18 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 1809
+ * 문제명: Moo
+ * 문제 링크: https://www.acmicpc.net/problem/1809
+ */
+public class BOJ_1809 {
+	public static void main(String[] args) {
+		System.out.println("(___)");
+		System.out.println("(o o)____/");
+		System.out.println(" @@      \\");
+		System.out.println("  \\ ____,/");
+		System.out.println("  //   //");
+		System.out.println(" ^^   ^^");
+	}
+}
