8eeee2e223b2dc6a5fc6cf0314cb241253170805
devkgn88 <devkgn88@gmail.com>
2025-05-20 11:35:43 +0900

    [김가남] 백준 10926 ??! 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10926.java
@@ -0,0 +1,18 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.util.Scanner;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10926
+ * 문제명: ??!
+ * 문제 링크: https://www.acmicpc.net/problem/10926
+ */
+public class BOJ_10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine().trim();
+		System.out.println(str+"??!");
+		sc.close();
+	}
+}
