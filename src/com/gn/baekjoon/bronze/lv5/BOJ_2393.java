d512fd555d74c2cdffd049e9589de36032044e1b
devkgn88 <devkgn88@gmail.com>
2025-05-21 16:26:53 +0900

    [김가남] 백준 2393 Rook 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_2393.java
@@ -0,0 +1,25 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 2393
+ * 문제명: Rook
+ * 문제 링크: https://www.acmicpc.net/problem/2393
+ */
+public class BOJ_2393 {
+    public static void main(String[] args){
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
