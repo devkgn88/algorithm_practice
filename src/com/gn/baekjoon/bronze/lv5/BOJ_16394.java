deea0a7aea30a578f264b07c9601e1fe35c20475
devkgn88 <devkgn88@gmail.com>
2025-05-22 14:25:05 +0900

    [김가남] 백준 16394 홍익대학교 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_16394.java
@@ -0,0 +1,18 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.util.Scanner;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 16394
+ * 문제명: 홍익대학교
+ * 문제 링크: https://www.acmicpc.net/problem/16394
+ */
+public class BOJ_16394 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(n-1946);
+		sc.close();
+	}
+}
