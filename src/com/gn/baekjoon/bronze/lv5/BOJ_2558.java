7d870c8a13a2298042f6689d17572bf062ea586c
devkgn88 <devkgn88@gmail.com>
2025-05-24 21:23:24 +0900

    [김가남] 백준 2558 A+B-2 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_2558.java
@@ -0,0 +1,19 @@
+package com.gn.baekjoon.bronze.lv5;
+
+import java.util.Scanner;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 2558
+ * 문제명: A+B-2
+ * 문제 링크: https://www.acmicpc.net/problem/2558
+ */
+public class BOJ_2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a+b);
+		sc.close();
+	}
+}
