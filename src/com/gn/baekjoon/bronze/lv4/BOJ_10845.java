9af41df794f050320ec66360a41339a8be907620
devkgn88 <devkgn88@gmail.com>
2025-06-02 12:29:06 +0900

    [김가남] 백준 10845 welcome 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv4/BOJ_10845.java
@@ -0,0 +1,10 @@
+package com.gn.baekjoon.bronze.lv4;
+
+public class BOJ_10845 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+	}
+}
