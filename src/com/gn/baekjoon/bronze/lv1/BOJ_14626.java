5c6b162a2e15a4a3fe6df6f642728495c34926ef
devkgn88 <devkgn88@gmail.com>
2025-06-07 10:16:33 +0900

    [김가남] ISBN 두번째 시도

--- src/com/gn/baekjoon/bronze/lv1/BOJ_14626.java
+++ src/com/gn/baekjoon/bronze/lv1/BOJ_14626.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 
 public class BOJ_14626 {
 	public static void main(String[] args) {
@@ -14,44 +13,43 @@ public class BOJ_14626 {
 			String[] arr = br.readLine().trim().split("");
 			int leng = arr.length;
 			
+			int sum = 0;
+			int idx = leng-1;
+			int multi = 1;
+			
+			for(int i = 0 ; i < arr.length-1 ; i++) {
+				if(arr[i].equals("*")) {
+					idx = i;
+					if(i%2 != 0) multi = 3;
+				}else {
+					if(i%2 == 0) {
+						sum += Integer.parseInt(arr[i]);
+					}else {
+						sum += Integer.parseInt(arr[i])*3;
+					}
+				}
+			}
+			
 			int result = 0;
 			
-			if(arr[leng-1].equals("*")) {
-				BigInteger sum = new BigInteger("0");
-				for(int i = 0 ; i < leng-1 ; i++) {
-					if(i%2==0) {
-						sum = sum.add(new BigInteger(arr[i]));
-					}else {
-						sum = sum.add(new BigInteger(arr[i]).multiply(new BigInteger("3")));
-					}
+			if(idx == leng-1) {
+				if(sum % 10 == 0 ) {
+					result = 0;
+				}else {
+					result = 10-(sum % 10);
+					
 				}
-				sum = sum.mod(new BigInteger("10"));
-				result = 10 - sum.intValue();
-			}else {
-				BigInteger rest = new BigInteger("10").subtract(new BigInteger(arr[leng-1]));
-				int multi = 1;
-				BigInteger sum = new BigInteger("0");
-				
-				for(int i = 0 ; i < leng-1 ; i++) {
-					if(arr[i].equals("*")) {
-						if(i%2 !=0) multi = 3;
-					}else {
-						if(i%2==0) {
-							sum = sum.add(new BigInteger(arr[i]));
-						}else {
-							sum = sum.add(new BigInteger(arr[i]).multiply(new BigInteger("3")));
+			} else {
+				if(10-(sum%10) == Integer.parseInt(arr[leng-1])) {
+					result = 0;
+				}else {
+					for(int i = 0 ; i <= 9 ; i++) {
+						if((sum + (multi*i)) % 10 == Integer.parseInt(arr[leng-1])) {
+							result = 10-i;
+							break;
 						}
 					}
-				}
-
-				for(int i = 0 ; i <= 9 ; i++) {
-					BigInteger gob = new BigInteger(String.valueOf(multi*i));
-					BigInteger temp = sum.add(gob);
 					
-					if(temp.remainder(new BigInteger("10")).compareTo(rest) == 0) {
-						result = i;
-						break;
-					}
 				}
 			}
 			
