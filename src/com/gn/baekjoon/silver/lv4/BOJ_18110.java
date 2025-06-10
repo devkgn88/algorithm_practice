775c3abd7fb5de741aa8994e39b39e57feb8f198
devkgn88 <devkgn88@gmail.com>
2025-06-10 17:06:49 +0900

    [김가남] 백준 18110 solved.ac 문제 풀이

--- src/com/gn/baekjoon/silver/lv4/BOJ_18110.java
+++ src/com/gn/baekjoon/silver/lv4/BOJ_18110.java
@@ -4,40 +4,45 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+/**
+ * 레벨 : 실버 4
+ * 문제 번호: 18110
+ * 문제명: solved.ac
+ * 문제 링크: https://www.acmicpc.net/problem/18110
+ */
 public class BOJ_18110 {
 	public static void main(String[] args) {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));){
 			
-			List<Integer> list = new ArrayList<Integer>();
-
+			int[] arr = new int[31];
+			
 			int n = Integer.parseInt(br.readLine().trim());
+			
 			for(int i = 0 ; i < n ; i++) {
 				int temp = Integer.parseInt(br.readLine().trim());
-				list.add(temp);
-			}
-					
-			Collections.sort(list);
-			
-			long num = Math.round(list.size()*0.15);
-			
-			for(int i = 0 ; i < num ; i++) {
-				list.remove(0);
-				list.remove(list.size()-1);
-			}
-
-			long sum = 0L;
-			
-			for(int i : list) {
-				sum += i;
+				arr[temp] = arr[temp] + 1;
 			}
 			
-			long avg = Math.round((double)sum / list.size());
+			long num = Math.round(n*0.15);
 			
+			int cnt = 0;
+			int sum = 0;
+			
+			
+			for(int i = 0 ; i < arr.length ; i++) {
+				if(arr[i] != 0) {
+					for(int j = 0 ; j < arr[i] ; j++) {
+						cnt ++;
+						if(num < cnt && cnt <= n-num ) {
+							sum += i;
+						}
+					}
+				}
+			}
+			
+			long avg = Math.round((double)sum / (n-num*2));
 			bw.write(String.valueOf(avg));
 			
 		}catch(Exception e) {
