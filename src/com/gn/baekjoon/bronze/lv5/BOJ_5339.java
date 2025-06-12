9e2f5ac1f2a74d5919192394a93bea6e8c11b1a7
devkgn88 <devkgn88@gmail.com>
2025-06-12 18:25:31 +0900

    [김가남] 백준 5339 콜센터 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_5339.java
@@ -0,0 +1,22 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 5339
+ * 문제명: 콜센터
+ * 문제 링크: https://www.acmicpc.net/problem/5339
+ */
+public class BOJ_5339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
