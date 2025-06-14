0290f4fa2dd317335916d2db742d6e51be952cc0
devkgn88 <devkgn88@gmail.com>
2025-06-14 22:17:24 +0900

    [김가남] 백준 10845 큐 문제 풀이

--- src/com/gn/baekjoon/silver/lv4/BOJ_10845.java
+++ src/com/gn/baekjoon/silver/lv4/BOJ_10845.java
@@ -7,6 +7,12 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * 레벨 : 실버 4
+ * 문제 번호: 10845
+ * 문제명: 큐
+ * 문제 링크: https://www.acmicpc.net/problem/10845
+ */
 public class BOJ_10845 {
 	public static void main(String[] args) {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,16 +25,11 @@ public class BOJ_10845 {
 				String[] arr = br.readLine().trim().split("\\s+");
 				switch(arr[0]) {
 					case "push" : d.offer(Integer.parseInt(arr[1]));break;
-					case "front" : bw.write(d.isEmpty() ? "-1" : d.getFirst()+"\n");break;
-					case "back" : bw.write(d.isEmpty() ? "-1" : d.getLast()+"\n");break;
+					case "front" : bw.write((d.isEmpty() ? "-1" : d.getFirst())+"\n");break;
+					case "back" : bw.write((d.isEmpty() ? "-1" : d.getLast())+"\n");break;
 					case "size" : bw.write(d.size()+"\n");break;
 					case "empty" : bw.write((d.isEmpty() ? "1" : "0")+"\n");break;
-					case "pop" : 
-						if(d.isEmpty()) {
-							bw.write("-1"+"\n");
-						}else {
-							bw.write(d.poll()+"\n");
-						}break;
+					case "pop" : bw.write((d.isEmpty() ? "-1" : d.poll())+"\n");
 				}
 			}	
 			bw.flush();
