c1bfaa0e79de729d19146dc1a137eb6ba1817b29
devkgn88 <devkgn88@gmail.com>
2025-06-28 21:26:49 +0900

    [김가남] 백준 25311 UCPC 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_25311.java
@@ -0,0 +1,13 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 25311
+ * 문제명: UCPC에서 가장 쉬운 문제 번호는?
+ * 문제 링크: https://www.acmicpc.net/problem/25311
+ */
+public class BOJ_25311 {
+	public static void main(String[] args) {
+		System.out.println("A");
+	}
+}
