4d596893c4f55d32d7d4146ddbad876e3f478185
devkgn88 <devkgn88@gmail.com>
2025-06-29 17:15:49 +0900

    [김가남] 백준 10718 강한친구 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10718.java
@@ -0,0 +1,14 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10718
+ * 문제명: We love kriii
+ * 문제 링크: https://www.acmicpc.net/problem/10718
+ */
+public class BOJ_10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
