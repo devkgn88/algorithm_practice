62192acacea3d27f47cbc2ae9be54207ff9ec2c3
devkgn88 <devkgn88@gmail.com>
2025-07-07 22:33:05 +0900

    [김가남] 백준 5338 마이크로소프트 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_5338.java
@@ -0,0 +1,17 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 5338
+ * 문제명: 마소
+ * 문제 링크: https://www.acmicpc.net/problem/5338
+ */
+public class BOJ_5338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
