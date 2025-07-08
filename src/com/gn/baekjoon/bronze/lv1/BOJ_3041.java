e4769136584f3b456e0c8380e02fea085e2a366f
devkgn88 <devkgn88@gmail.com>
2025-07-08 16:40:01 +0900

    [김가남] 백준 N-퍼즐 코드 개선

--- src/com/gn/baekjoon/bronze/lv1/BOJ_3041.java
+++ src/com/gn/baekjoon/bronze/lv1/BOJ_3041.java
@@ -18,22 +18,18 @@ public class BOJ_3041 {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));){
 			
+
 			Map<Character, Point> map = new HashMap<Character,Point>();
-			map.put('A', new Point(0,0));
-			map.put('B', new Point(0,1));
-			map.put('C', new Point(0,2));
-			map.put('D', new Point(0,3));
-			map.put('E', new Point(1,0));
-			map.put('F', new Point(1,1));
-			map.put('G', new Point(1,2));
-			map.put('H', new Point(1,3));
-			map.put('I', new Point(2,0));
-			map.put('J', new Point(2,1));
-			map.put('K', new Point(2,2));
-			map.put('L', new Point(2,3));
-			map.put('M', new Point(3,0));
-			map.put('N', new Point(3,1));
-			map.put('O', new Point(3,2));
+			
+			int alpha = 65;
+			for(int i = 0 ; i < 4; i++) {
+				for(int j = 0 ; j < 4 ; j++) {
+					if(65 <= alpha && alpha <= 79) {
+						map.put((char)alpha, new Point(i,j));
+						alpha++;
+					}
+				}
+			}
 			
 			int sum = 0;
 			
