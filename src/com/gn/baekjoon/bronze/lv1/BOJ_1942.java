5cd09e4865849581f9f8ce84420242086dc65319
devkgn88 <devkgn88@gmail.com>
2025-07-09 16:40:01 +0900

    [김가남] 백준 1942 디지털 시계 정답

--- src/com/gn/baekjoon/bronze/lv1/BOJ_1942.java
+++ src/com/gn/baekjoon/bronze/lv1/BOJ_1942.java
@@ -7,6 +7,12 @@ import java.io.OutputStreamWriter;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * 레벨 : 브론즈 2
+ * 문제 번호: 1942
+ * 문제명: 디지털시계
+ * 문제 링크: https://www.acmicpc.net/problem/1942
+ */
 public class BOJ_1942 {
 	public static void main(String[] args) {
 		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,26 +26,30 @@ public class BOJ_1942 {
 				LocalTime end = LocalTime.parse(arr[1],dtf);
 				int count = 0;
 				
-				while(true) {
-					int hour = start.getHour();
-					int min = start.getMinute();
-					int second = start.getSecond();
-					
-					String time = hour+""+
-								(min<10 ? "0"+min : min)+""+
-								(second<10 ? "0"+second : second)+"";
-					
-					int num = Integer.parseInt(time);
-					if(num % 3 == 0) count++;		
-							
-					start = start.plusSeconds(1);
-					if(start.equals(end)) break;
-				}
-				bw.write(count+"\n");
-			}
+                do {
+                    int hour = start.getHour();
+                    int min = start.getMinute();
+                    int second = start.getSecond();
+
+                    String time = hour+""+
+							(min<10 ? "0"+min : min)+""+
+							(second<10 ? "0"+second : second)+"";
+				
+                    int num = Integer.parseInt(time);
+                    if(num % 3 == 0) count++;		
+						
+
+                    start = start.plusSeconds(1);
+                } while (!start.equals(end.plusSeconds(1)));
+
+                bw.write(count + "\n");
+   			}
+			
+			bw.flush();
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
