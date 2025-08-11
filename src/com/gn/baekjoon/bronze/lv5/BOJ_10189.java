5edc7e027767210d3e13c839d1834bc3a670be29
devkgn88 <devkgn88@gmail.com>
2025-08-11 23:16:32 +0900

    [김가남] 백준 10189 Hook 문제 풀이

---
+++ src/com/gn/baekjoon/bronze/lv5/BOJ_10189.java
@@ -0,0 +1,15 @@
+package com.gn.baekjoon.bronze.lv5;
+
+/**
+ * 레벨 : 브론즈 5
+ * 문제 번호: 10189
+ * 문제명: Hook
+ */
+public class BOJ_10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+    }
+}
